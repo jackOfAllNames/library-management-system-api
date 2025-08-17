3c79cdb8b70762e7cb7cb4cb94c3a1a0e20510d3
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-17 21:48:21 +0100

    returned list of all books endpoint

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookListSerialized, HomePage
+from .views import BookListView, HomePage
 
 urlpatterns = [
     path('', HomePage, name='home'),
+    path('books/', BookListView.as_view(), name='books'),
 ]
--- book/views.py
+++ book/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from django.http import JsonResponse
+from rest_framework import generics
 
 def BookListSerialized(request):
     books = Book.objects.all()
@@ -11,3 +12,8 @@ def BookListSerialized(request):
 
 def HomePage(request):
     return JsonResponse({"message": "Welcome to the Book API!"})
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
