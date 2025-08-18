6f1c6281fb08519547156b3f3a44e85ae532abeb
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-18 14:42:36 +0100

    created the user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLES = (
+        ('admin', 'Admin'),
+        ('member', 'Member'),
+    )
+
+    role = models.CharField(max_length=10, choices=ROLES, default='member')
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
