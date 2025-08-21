1cc20f12ebd2a8c27bb4d664fa50669e7ef33df1
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-21 14:20:18 +0100

    A user can login and token will be provided

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import HomePage, RegisterView
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 router = DefaultRouter()
@@ -9,4 +10,5 @@ router = DefaultRouter()
 urlpatterns = [
     # path('', include(router.urls)),
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', obtain_auth_token, name='login'),
 ]
