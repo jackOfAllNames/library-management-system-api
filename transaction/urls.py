d95e0665680b76d2a5a74fd83fff60cc65e610a4
Olaitan Adedokun <kenzdex@gmail.com>
2025-09-05 17:26:36 +0100

    return a book successfully

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import checkout_book
+from .views import checkout_book, return_book
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -8,8 +8,8 @@ router = DefaultRouter()
 # router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    # path('users/login/', obtain_auth_token, name='login'),
     path('checkout/<int:book_id>/', checkout_book, name='checkout_book'),
+    path('return/<int:book_id>/', return_book, name='return_book'),
     # path('', include(router.urls)),
     # path('', ListUsersView.as_view(), name='list_users'),
     # path('register/', RegisterView.as_view(), name='register'),
--- transaction/views.py
+++ transaction/views.py
@@ -10,7 +10,7 @@ class BookCheckoutView(ListAPIView):
     serializer_class = TransactionSerializer
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def checkout_book(request, book_id):
     try:
         book = Book.objects.get(id=book_id)
@@ -24,3 +24,19 @@ def checkout_book(request, book_id):
     book.save()
 
     return Response({"message": f"Book '{book.title}' checked out successfully"}, status=status.HTTP_200_OK)    
+
+
+@api_view(['POST'])
+def return_book(request, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        return Response({"error": f"Book with id - {book_id} not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    if book.available_copies >= book.total_copies:
+        return Response({"error": "All copies are already in the library"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    book.available_copies += 1
+    book.save()
+
+    return Response({"message": f"Book '{book.title}' returned successfully"}, status=status.HTTP_200_OK)
